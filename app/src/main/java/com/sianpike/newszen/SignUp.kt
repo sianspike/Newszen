8421fe7000edac74d2a766637b8138622c266709
Sian Pike <sian.pike@icloud.com>
2020-11-03 20:29:27 +0000

    Adding firebase to sign up activity

--- app/src/main/java/com/sianpike/newszen/SignUp.kt
+++ app/src/main/java/com/sianpike/newszen/SignUp.kt
@@ -4,11 +4,25 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
 class SignUp : AppCompatActivity() {
+
+    lateinit var mAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+        mAuth = FirebaseAuth.getInstance()
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        var currentUser: FirebaseUser = mAuth.getCurrentUser()
+        updateUI(currentUser)
     }
 
     fun loginButtonClicked(view: View) {
