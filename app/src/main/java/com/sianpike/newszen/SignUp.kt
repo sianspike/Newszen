6c14bb18c85bb6f52767e1c5862a8f32e8f16d8c
Sian Pike <sian.pike@icloud.com>
2020-11-06 10:29:45 +0000

    User can now sign up

--- app/src/main/java/com/sianpike/newszen/SignUp.kt
+++ app/src/main/java/com/sianpike/newszen/SignUp.kt
@@ -3,26 +3,36 @@ package com.sianpike.newszen
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.TextView
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.ktx.Firebase
 
 class SignUp : AppCompatActivity() {
 
-    lateinit var mAuth: FirebaseAuth
+    private var auth = FirebaseAuth.getInstance()
+    private val TAG = "Idontknow"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
-
-        mAuth = FirebaseAuth.getInstance()
     }
 
-    override fun onStart() {
+    public override fun onStart() {
         super.onStart()
 
-        var currentUser: FirebaseUser = mAuth.getCurrentUser()
-        updateUI(currentUser)
+        val dashboard = Intent(this, Dashboard::class.java)
+
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+
+        if (currentUser != null) {
+
+            startActivity(dashboard)
+            //updateUI(currentUser)
+        }
     }
 
     fun loginButtonClicked(view: View) {
@@ -33,7 +43,26 @@ class SignUp : AppCompatActivity() {
 
     fun signUpButtonClicked(view: View) {
 
+        var email = findViewById<TextView>(R.id.emailField).text.toString()
+        var password = findViewById<TextView>(R.id.passwordField).text.toString()
+
         val topics = Intent(this, Topics::class.java)
-        startActivity(topics)
+
+        auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d(TAG, "createUserWithEmail:success")
+                        val user = auth.currentUser
+                        startActivity(topics)
+                        //updateUI(user)
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                        Toast.makeText(baseContext, "Authentication failed.",
+                                Toast.LENGTH_SHORT).show()
+                        //updateUI(null)
+                    }
+                }
     }
 }
