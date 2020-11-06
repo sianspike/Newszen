44482254fa5cd59ffba96f4cacd024698330ab74
Sian Pike <sian.pike@icloud.com>
2020-11-06 13:59:08 +0000

    Changed topics to real-world avilable topics and ensuring buttons have full functionality in topics

--- app/src/main/java/com/sianpike/newszen/Login.kt
+++ app/src/main/java/com/sianpike/newszen/Login.kt
@@ -3,12 +3,39 @@ package com.sianpike.newszen
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.TextView
+import android.widget.Toast
+import com.google.android.material.tabs.TabLayout
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class Login : AppCompatActivity() {
+
+    private var mAuth = FirebaseAuth.getInstance()
+    private val tag = "Login"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+    }
+
+    public override fun onStart() {
+        super.onStart()
+
+//        val dashboard = Intent(this, Dashboard::class.java)
+//        // Check if user is signed in (non-null) and update UI accordingly.
+//        val currentUser = auth.currentUser
+//
+//        if (currentUser != null) {
+//
+//            startActivity(dashboard)
+//            //updateUI(currentUser)
+//        }
+
+        Firebase.auth.signOut()
     }
 
     fun signUpButtonClicked(view: View) {
@@ -19,7 +46,26 @@ class Login : AppCompatActivity() {
 
     fun loginButtonClicked(view: View) {
 
+        var email = findViewById<TextView>(R.id.emailField).text.toString()
+        var password = findViewById<TextView>(R.id.passwordField).text.toString()
+
         val dashboard = Intent(this, Dashboard::class.java)
-        startActivity(dashboard)
+
+        mAuth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(tag, "signInWithEmail:success")
+                    val user = mAuth.currentUser
+                    startActivity(dashboard)
+                    //updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(tag, "signInWithEmail:failure", task.exception)
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+                    //updateUI(null)
+                }
+            }
     }
 }
--- app/src/main/java/com/sianpike/newszen/Topics.kt
+++ app/src/main/java/com/sianpike/newszen/Topics.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 
 class Topics : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,5 +16,36 @@ class Topics : AppCompatActivity() {
 
         val dashboard = Intent(this, Dashboard::class.java)
         startActivity(dashboard)
+    }
+
+    fun businessButtonClicked(view: View) {
+
+    }
+
+    fun entertainmentButtonClicked(view: View) {
+
+    }
+
+    fun generalButtonClicked(view: View) {
+
+    }
+
+    fun healthButtonClicked(view: View) {
+
+    }
+
+    fun scienceButtonClicked(view: View) {
+
+    }
+
+    fun sportsButtonClicked(view: View) {
+
+    }
+
+    fun technologyButtonClicked(view: View) {
+
+        var button = findViewById<Button>(R.id.technologyButton)
+
+        button.isSelected = button.isSelected == false
     }
 }
